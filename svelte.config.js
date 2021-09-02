5d69ae8aabce726486116661402b32fdb690f0c7
Rodney Lab <codesign@rodneylab.com>
2021-09-02 11:10:03 +0100

    fix(svelte.config.js): use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/static';
+import adapter from '@sveltejs/adapter-static';
 import { imagetools } from 'vite-imagetools';
 import { mdsvex } from 'mdsvex';
 import preprocess from 'svelte-preprocess';
